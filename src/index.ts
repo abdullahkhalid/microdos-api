b24b71543b203c685edf06a9b61aeb05d8bc1858
Abdullah Khalid <abdullah@frontier-instruments.com>
2025-10-29 19:12:29 +0500

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000
 app.use(helmet())
 app.use(
 	cors({
-		origin: [process.env.APP_BASE_URL || 'https://microdos-web.vercel.app', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5173', 'https://microdos.in', 'https://www.microdos.in'],
+		origin: [process.env.APP_BASE_URL || 'https://microdos-web-inky.vercel.app', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5173', 'https://microdos.in', 'https://www.microdos.in'],
 		credentials: true
 	})
 )
